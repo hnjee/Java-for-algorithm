d9864a9d89177fa5bbabbc0f8888707285304e07
hnjee <leehnjee@gmail.com>
2024-11-07 13:50:20 +0900

    1-1. 문자찾기

--- src/String/Solution_1.java
+++ src/String/Solution_1.java
@@ -1,26 +1,24 @@
 package String;
 
+import java.util.Scanner;
+
+//1-1. 문자찾기
 public class Solution_1 {
     public int solution(String str, char c){
         int answer = 0;
         str = str.toUpperCase();
         c = Character.toUpperCase(c);
-        //기본 for문
-//        for(int i=0; i<str.length(); i++){
-//            if(str.charAt(i)==c) answer++;
-//        }dd
-
         //향상된 for문
         for(char x : str.toCharArray()){
             if(x==c) answer++;
         }
         return answer;
     }
-//    public static void main(String[] args){
-//        Main T = new Main();
-//        Scanner kb = new Scanner(System.in);
-//        String str = kb.next();
-//        char c = kb.next().charAt(0);
-//        System.out.println(T.solution(str, c));
-//    }
+    public static void main(String[] args){
+        Solution_1 T = new Solution_1();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+        //System.out.println(T.solution(str, c));
+    }
 }
