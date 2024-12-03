e4577afda71592ed7f5b55edd1e5e1886ac3edee
hnjee <leehnjee@gmail.com>
2024-12-03 20:53:39 +0900

    2-12

---
+++ src/Array/Solution12.java
@@ -0,0 +1,40 @@
+package Array;
+import java.util.Scanner;
+
+//2-12. 멘토링 (answer 배열 추가해서 시간복잡도 낮추기)
+public class Solution12 {
+    public int solution(int n, int m, int[][] arr){
+        int[][] answer = new int[n][n];
+        int cnt = 0;
+        for(int i=0; i<m; i++){
+            for(int j=1; j<n; j++){
+                int x = arr[i][j];
+                for(int k=j-1; k>=0; k--){
+                    int y = arr[i][k];
+                    answer[x-1][y-1] = 1;
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i==j) answer[i][j] = 1;
+                if(answer[i][j]==0) cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args){
+        Solution12 T = new Solution12();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int m=kb.nextInt();
+        int[][] arr=new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                arr[i][j]=kb.nextInt();
+            }
+        }
+        System.out.print(T.solution(n, m, arr));
+    }
+}
---
+++ src/Array/Solution12_2.java
@@ -0,0 +1,42 @@
+package Array;
+import java.util.Scanner;
+
+//2-12. 멘토링 (4중 for문 - 하나하나 비교)
+public class Solution12_2 {
+    public int solution(int n, int m, int[][] arr){
+        int answer = 0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                //(i-멘토, j-멘티) 모든 경우의 수
+                int cnt=0;
+                for(int x=0; x<m; x++){
+                    int i_rank=0, j_rank=0;
+                    for(int y=0; y<n; y++){
+                        //arr 탐색하며 i와 j의 등수 저장
+                        if(arr[x][y]==i) i_rank=y;
+                        if(arr[x][y]==j) j_rank=y;
+                    }
+                    //i의 등수가 j의 등수보다 높으면(작으면) cnt++
+                    if(i_rank<j_rank) cnt++;
+                }
+                //i의 등수가 j의 등수보다 높은 경우가 m번(매번)이면 answer++
+                if(cnt==m) answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Solution12_2 T = new Solution12_2();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int m=kb.nextInt();
+        int[][] arr=new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                arr[i][j]=kb.nextInt();
+            }
+        }
+        System.out.print(T.solution(n, m, arr));
+    }
+}
